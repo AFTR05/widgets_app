06cfe2148bcc978f201a245fd359ee3d6e37556f
Andres <andrestoro0303@gmail.com>
2025-01-03 16:59:23 -0500

    feat snackbars and dialogs

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/config/router/app_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = "snackbar_screen";
@@ -16,11 +18,50 @@ class SnackbarScreen extends StatelessWidget {
       );
     }
 
+    void openDialog(BuildContext context){
+      showDialog(
+        context: context, 
+        barrierDismissible: false,
+        builder: (context) => AlertDialog(
+          title: const Text('Estas seguro'),
+          content: const Text('Lorem ipsum dolor sit amet, consectetur'),
+          actions: [
+            TextButton(onPressed: () => context.pop(), child: const Text('Cancelar')),
+            FilledButton(onPressed: () => context.pop() , child: const Text('Aceptar')),
+          ],
+      ));
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y dialogos'),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: (){
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text('lorem ipsum dolor sit amet, consectetur lorem ipsum dolor lorem ipsum dolor lorem ipsum')
+                  ]
+                );
+              }, 
+              child: const Text('Licencias usadas')
+            ),
+            FilledButton.tonal(
+              onPressed: (){
+                openDialog(context);
+              }, 
+              child: const Text('Mostrar dialogo')
+            )
+          ],
+        ),
       ),
 
       floatingActionButton: FloatingActionButton.extended(
