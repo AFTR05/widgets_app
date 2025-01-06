f53a7324cbb90bf57b8e0b1dc06a89831e6016f2
Andres <andrestoro0303@gmail.com>
2025-01-05 21:34:29 -0500

    feat menu with notch validation

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
 
@@ -9,28 +11,38 @@ class SideMenu extends StatefulWidget {
 }
 
 class _SideMenuState extends State<SideMenu> {
-
   int navDrawerIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
-      selectedIndex: navDrawerIndex,
-      onDestinationSelected: (value) {
-        setState(() {
-          navDrawerIndex = value;
-        });
-      },
-      children: const [
-        NavigationDrawerDestination(
-          icon: Icon(Icons.add), 
-          label: Text('Home Screen')
-        ),
-        NavigationDrawerDestination(
-          icon: Icon(Icons.add_shopping_cart_rounded), 
-          label: Text('Otra pantalla')
-        )
-      ]
-    );
+        selectedIndex: navDrawerIndex,
+        onDestinationSelected: (value) {
+          setState(() {
+            navDrawerIndex = value;
+          });
+        },
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20, 16, 10),
+            child: const Text('Main'),
+          ),
+          ...appMenuItems.sublist(0, 3).map((item) =>
+              NavigationDrawerDestination(
+                  icon: Icon(item.icon), label: Text(item.title))),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+            child: Divider()
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 10, 16, 10),
+            child: Text('More Options'),
+          ),
+          ...appMenuItems.sublist(3).map((item) =>
+              NavigationDrawerDestination(
+                  icon: Icon(item.icon), label: Text(item.title))),
+        ]);
   }
 }
