3eb38e63daf1c7c2c2cec55f85e2ae01e90a8c11
Andres <andrestoro0303@gmail.com>
2025-01-08 20:37:03 -0500

    feat colors riverpod provider

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,4 +1,8 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 final isDarkModeProvider = StateProvider<bool>((ref) => true);
+
+// Listado de colores inmulables
+final colorListProvider = Provider((ref) => colorList);
